e7646a94ee7c4c3bc732f5fbe544b6bc474af6b9
ahmadsayeb <ahmadsayeb3@gmail.com>
2020-04-22 20:20:25 -0400

    npm scripts

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$("#carouselButton").click(function(){
+    if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carouselButton").children("span").removeClass('fa-pause');
+        $("#carouselButton").children("span").addClass('fa-play');
+    }
+    else if ($("#carouselButton").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carouselButton").children("span").removeClass('fa-play');
+        $("#carouselButton").children("span").addClass('fa-pause');                    
+    }
+});
+$("#loginButton").click(function(){
+$("#loginModal").modal({
+keyboard: true
+})
+
+});
+$("#closeLogin").click(function(){
+$("#loginModal").modal('hide');
+
+});
+$("#cancelLogin").click(function(){
+$("#loginModal").modal('hide');
+});
+$("#reserveButton").click(function(){
+$("#reserve").modal({
+keyboard: true
+})
+});
+$("#closeReserve").click(function(){
+$("#reserve").modal('hide');
+});
+$("#closeModalFooter").click(function(){
+$("#reserve").modal('hide');
+});
